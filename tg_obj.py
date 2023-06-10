f2d8f97412bda436052cbd99dbbe7bbf3a27a7c0
Sergryap <rs1180@mail.ru>
2023-06-10 17:12:29 +0500

    The annotate improvements

--- tg_obj.py
+++ tg_obj.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import httpx
 
 from pydantic import BaseModel as GeneralBaseModel, root_validator, AnyHttpUrl
-from typing import Dict, List, Any, Union
+from typing import Any, Union
 
 
 class BaseModel(GeneralBaseModel):
@@ -49,8 +49,8 @@ class Chat(BaseModel):
     first_name: str = None
     last_name: str = None
     is_forum: bool = None
-    photo: Dict[str, Any] = None
-    active_usernames: List[str] = None
+    photo: dict[str, Any] = None
+    active_usernames: list[str] = None
     emoji_status_custom_emoji_id: str = None
     bio: str = None
     has_private_forwards: bool = None
@@ -60,7 +60,7 @@ class Chat(BaseModel):
     description: str = None
     invite_link: str = None
     pinned_message: Message = None
-    permissions: Dict[str, Any] = None
+    permissions: dict[str, Any] = None
     slow_mode_delay: int = None
     message_auto_delete_time: int = None
     has_aggressive_anti_spam_enabled: bool = None
@@ -81,12 +81,12 @@ class KeyboardButton(BaseModel):
     """
 
     text: str
-    request_user: Dict[str, Union[int, bool]] = None
-    request_chat: Dict[str, Union[int, bool, Dict[str, bool]]] = None
+    request_user: dict[str, Union[int, bool]] = None
+    request_chat: dict[str, Union[int, bool, dict[str, bool]]] = None
     request_contact: bool = None
     request_location: bool = None
-    request_poll: Dict[str, str] = None
-    web_app: Dict[str, AnyHttpUrl] = None
+    request_poll: dict[str, str] = None
+    web_app: dict[str, AnyHttpUrl] = None
 
 
 class ReplyKeyboardMarkup(BaseModel):
@@ -95,7 +95,7 @@ class ReplyKeyboardMarkup(BaseModel):
     See here: https://core.telegram.org/bots/api#replykeyboardmarkup
     """
 
-    keyboard: List[List[KeyboardButton]]
+    keyboard: list[list[KeyboardButton]]
     is_persistent: bool = None
     resize_keyboard: bool = None
     one_time_keyboard: bool = None
@@ -137,11 +137,11 @@ class InlineKeyboardButton(BaseModel):
     text: str
     url: str = None
     callback_data: str = None
-    web_app: Dict[str, AnyHttpUrl] = None
-    login_url: Dict[str, Union[str, bool]] = None
+    web_app: dict[str, AnyHttpUrl] = None
+    login_url: dict[str, Union[str, bool]] = None
     switch_inline_query: str = None
     switch_inline_query_current_chat: str = None
-    switch_inline_query_chosen_chat: Dict[str, Union[str, bool]] = None
+    switch_inline_query_chosen_chat: dict[str, Union[str, bool]] = None
     callback_game: Any = None
     pay: bool = None
 
@@ -152,7 +152,7 @@ class InlineKeyboardMarkup(BaseModel):
     See here: https://core.telegram.org/bots/api#inlinekeyboardmarkup
     """
 
-    inline_keyboard: List[List[InlineKeyboardButton]]
+    inline_keyboard: list[list[InlineKeyboardButton]]
 
 
 class Invoice(BaseModel):
@@ -227,7 +227,7 @@ class Message(BaseModel):
     forward_signature: str = None
     forward_sender_name: str = None
     forward_date: int = None
-    is_topic_message: Dict[str, Any] = None
+    is_topic_message: dict[str, Any] = None
     is_automatic_forward: bool = None
     reply_to_message: 'Message' = None
     via_bot: User = None
@@ -236,55 +236,55 @@ class Message(BaseModel):
     media_group_id: str = None
     author_signature: str = None
     text: str = None
-    entities: List[MessageEntity] = None
-    animation: Dict[str, Any] = None
-    audio: Dict[str, Any] = None
-    document: Dict[str, Any] = None
-    photo: List[Dict[str, Any]] = None
-    sticker: Dict[str, Any] = None
-    video: Dict[str, Any] = None
-    video_note: Dict[str, Any] = None
-    voice: Dict[str, Any] = None
+    entities: list[MessageEntity] = None
+    animation: dict[str, Any] = None
+    audio: dict[str, Any] = None
+    document: dict[str, Any] = None
+    photo: list[dict[str, Any]] = None
+    sticker: dict[str, Any] = None
+    video: dict[str, Any] = None
+    video_note: dict[str, Any] = None
+    voice: dict[str, Any] = None
     caption: str = None
-    caption_entities: List[MessageEntity] = None
+    caption_entities: list[MessageEntity] = None
     has_media_spoiler: bool = None
-    contact: Dict[str, Any] = None
-    dice: Dict[str, Any] = None
-    game: Dict[str, Any] = None
-    poll: Dict[str, Any] = None
-    venue: Dict[str, Any] = None
-    location: Dict[str, Any] = None
-    new_chat_members: List[User] = None
+    contact: dict[str, Any] = None
+    dice: dict[str, Any] = None
+    game: dict[str, Any] = None
+    poll: dict[str, Any] = None
+    venue: dict[str, Any] = None
+    location: dict[str, Any] = None
+    new_chat_members: list[User] = None
     left_chat_member: User = None
     new_chat_title: str = None
-    new_chat_photo: List[Dict[str, Any]] = None
+    new_chat_photo: list[dict[str, Any]] = None
     delete_chat_photo: bool = None
     group_chat_created: bool = None
     supergroup_chat_created: bool = None
     channel_chat_created: bool = None
-    message_auto_delete_timer_changed: List[Dict[str, Any]] = None
+    message_auto_delete_timer_changed: list[dict[str, Any]] = None
     migrate_to_chat_id: int = None
     migrate_from_chat_id: int = None
     pinned_message: 'Message' = None
     invoice: Invoice = None
     successful_payment: SuccessfulPayment = None
-    user_shared: Dict[str, Any] = None
-    chat_shared: Dict[str, Any] = None
+    user_shared: dict[str, Any] = None
+    chat_shared: dict[str, Any] = None
     connected_website: str = None
-    write_access_allowed: Dict[str, Any] = None
-    passport_data: Dict[str, Any] = None
-    proximity_alert_triggered: Dict[str, Any] = None
-    forum_topic_created: Dict[str, Any] = None
-    forum_topic_edited: Dict[str, Any] = None
-    forum_topic_closed: Dict[str, Any] = None
-    forum_topic_reopened: Dict[str, Any] = None
-    general_forum_topic_hidden: Dict[str, Any] = None
-    general_forum_topic_unhidden: Dict[str, Any] = None
-    video_chat_scheduled: Dict[str, Any] = None
-    video_chat_started: Dict[str, Any] = None
-    video_chat_ended: Dict[str, Any] = None
-    video_chat_participants_invited: Dict[str, Any] = None
-    web_app_data: Dict[str, Any] = None
+    write_access_allowed: dict[str, Any] = None
+    passport_data: dict[str, Any] = None
+    proximity_alert_triggered: dict[str, Any] = None
+    forum_topic_created: dict[str, Any] = None
+    forum_topic_edited: dict[str, Any] = None
+    forum_topic_closed: dict[str, Any] = None
+    forum_topic_reopened: dict[str, Any] = None
+    general_forum_topic_hidden: dict[str, Any] = None
+    general_forum_topic_unhidden: dict[str, Any] = None
+    video_chat_scheduled: dict[str, Any] = None
+    video_chat_started: dict[str, Any] = None
+    video_chat_ended: dict[str, Any] = None
+    video_chat_participants_invited: dict[str, Any] = None
+    web_app_data: dict[str, Any] = None
     reply_markup: InlineKeyboardMarkup = None
 
 
