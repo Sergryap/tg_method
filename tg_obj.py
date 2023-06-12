4df67c5d00016f9abc1b5d89de18a9ccbf2344c0
Sergryap <rs1180@mail.ru>
2023-06-12 14:23:36 +0500

    The model that represents an incoming update was added

--- tg_obj.py
+++ tg_obj.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import httpx
 
 from pydantic import BaseModel as GeneralBaseModel, root_validator, AnyHttpUrl, Field
-from typing import Any, Union
+from typing import Any, Union, TypeVar
 
 
 class BaseModel(GeneralBaseModel):
@@ -73,7 +73,7 @@ class Chat(BaseModel):
 
 class KeyboardButton(BaseModel):
     """This model represents one button of the reply keyboard.
-    For simple text buttons, String can be used instead of this object to specify the button text.
+    For simple text buttons, String can be used instead of this model to specify the button text.
     The optional fields web_app, request_user, request_chat, request_contact,
     request_location, and request_poll are mutually exclusive.
 
@@ -104,7 +104,7 @@ class ReplyKeyboardMarkup(BaseModel):
 
 
 class ReplyKeyboardRemove(BaseModel):
-    """Upon receiving a message with this object, Telegram clients will remove the current
+    """Upon receiving a message with this model, Telegram clients will remove the current
     custom keyboard and display the default letter-keyboard. By default, custom keyboards
     are displayed until a new keyboard is sent by a bot. An exception is made for one-time
     keyboards that are hidden immediately after the user presses a button
@@ -116,7 +116,7 @@ class ReplyKeyboardRemove(BaseModel):
 
 
 class ForceReply(BaseModel):
-    """Upon receiving a message with this object, Telegram clients will display a reply
+    """Upon receiving a message with this model, Telegram clients will display a reply
     interface to the user (act as if the user has selected the bot's message and tapped 'Reply').
     This can be extremely useful if you want to create user-friendly step-by-step interfaces
     without having to sacrifice privacy mode.
@@ -169,7 +169,7 @@ class Invoice(BaseModel):
 
 
 class SuccessfulPayment(BaseModel):
-    """This object contains basic information about a successful payment.
+    """This model contains basic information about a successful payment.
 
     See here: https://core.telegram.org/bots/api#successfulpayment
     """
@@ -184,7 +184,7 @@ class SuccessfulPayment(BaseModel):
 
 
 class OrderInfo(BaseModel):
-    """This object represents information about an order.
+    """This model represents information about an order.
 
     See here: https://core.telegram.org/bots/api#orderinfo
     """
@@ -196,7 +196,7 @@ class OrderInfo(BaseModel):
 
 
 class ShippingAddress(BaseModel):
-    """This object represents a shipping address.
+    """This model represents a shipping address.
 
     See here: https://core.telegram.org/bots/api#shippingaddress
     """
@@ -302,6 +302,308 @@ class MessageEntity(BaseModel):
     user: User = None
     language: str = None
     custom_emoji_id: str = None
+
+
+class Update(BaseModel):
+    """This model represents an incoming update.
+    At most one of the optional parameters can be present in any given update.
+
+    See here: https://core.telegram.org/bots/api#update
+    """
+
+    update_id: int
+    message: Message = None
+    edited_message: Message = None
+    edited_channel_post: Message = None
+    inline_query: InlineQuery = None
+    chosen_inline_result: ChosenInlineResult = None
+    callback_query: CallbackQuery = None
+    shipping_query: ShippingQuery = None
+    pre_checkout_query: PreCheckoutQuery = None
+    poll: Poll = None
+    poll_answer: PollAnswer = None
+    my_chat_member: ChatMemberUpdated = None
+    chat_member: ChatMemberUpdated = None
+    chat_join_request: ChatJoinRequest = None
+
+
+class InlineQuery(BaseModel):
+    """This model represents an incoming inline query.
+    When the user sends an empty query, your bot could return some default or trending results.
+
+    See here: https://core.telegram.org/bots/api#inlinequery
+    """
+
+    id: str
+    from_: User = Field(alias='from')
+    query: str = Field(max_length=256)
+    offset: str
+    chat_type: str = None
+    location: Location = None
+
+
+class Location(BaseModel):
+    """This model represents a point on the map.
+
+    See here: https://core.telegram.org/bots/api#location
+    """
+
+    longitude: float
+    latitude: float
+    horizontal_accuracy: float = Field(default=None, ge=0, le=1500)
+    live_period: int = None
+    heading: int = Field(default=None, ge=1, le=360)
+    proximity_alert_radius: int = None
+
+
+class ChosenInlineResult(BaseModel):
+    """Represents a result of an inline query that was chosen by the user and sent to their chat partner.
+
+    See here: https://core.telegram.org/bots/api#choseninlineresult
+    """
+
+    result_id: int
+    from_: User = Field(alias='from')
+    location: Location = None
+    inline_message_id: str = None
+    query: str = None
+
+
+class CallbackQuery(BaseModel):
+    """This model represents an incoming callback query from a callback button in an inline keyboard.
+    If the button that originated the query was attached to a message sent by the bot,
+    the field message will be present. If the button was attached to a message sent via the bot (in inline mode),
+    the field inline_message_id will be present. Exactly one of the fields data or game_short_name will be present.
+
+    See here: https://core.telegram.org/bots/api#callbackquery
+    """
+
+    id: str
+    from_: User = Field(alias='from')
+    message: Message = None
+    inline_message_id: str = None
+    chat_instance: str = None
+    data: str = None
+    game_short_name: str = None
+
+
+class ShippingQuery(BaseModel):
+    """This model contains information about an incoming shipping query.
+
+    See here: https://core.telegram.org/bots/api#shippingquery
+    """
+
+    id: str = None
+    from_: User = Field(alias='from')
+    invoice_payload: str
+    shipping_address: ShippingAddress
+
+
+class PreCheckoutQuery(BaseModel):
+    """This model contains information about an incoming pre-checkout query.
+
+    See here: https://core.telegram.org/bots/api#precheckoutquery
+    """
+
+    id: str
+    from_: User = Field(alias='from')
+    currency: str
+    total_amount: int
+    invoice_payload: str
+    shipping_option_id: str = None
+    order_info: OrderInfo = None
+
+
+class Poll(BaseModel):
+    """This model contains information about a poll.
+
+    See here: https://core.telegram.org/bots/api#poll
+    """
+
+    id: str
+    question: str
+    options: list[PollOption]
+    total_voter_count: int
+    is_closed: bool
+    is_anonymous: bool
+    type: str
+    allows_multiple_answers: bool
+    correct_option_id: int = None
+    explanation: str = None
+    explanation_entities: list[MessageEntity] = None
+    open_period: int = None
+    close_date: int = None
+
+
+class PollOption(BaseModel):
+    """This model contains information about one answer option in a poll.
+
+    See here: https://core.telegram.org/bots/api#polloption
+    """
+
+    text: str = Field(min_length=1, max_length=100)
+    voter_count: int = Field(ge=0)
+
+
+class PollAnswer(BaseModel):
+    """This model represents an answer of a user in a non-anonymous poll.
+
+    See here: https://core.telegram.org/bots/api#pollanswer
+    """
+
+    poll_id: str
+    user: User
+    option_ids: list[Union[int, None]]
+
+
+class ChatMemberUpdated(BaseModel):
+    """This model represents changes in the status of a chat member.
+
+    See here: https://core.telegram.org/bots/api#chatmemberupdated
+    """
+
+    chat: Chat
+    from_: User = Field(alias='from')
+    date: int
+    old_chat_member: ChatMember
+    new_chat_member: ChatMember
+    invite_link: ChatInviteLink = None
+    via_chat_folder_invite_link: bool = None
+
+
+class ChatInviteLink(BaseModel):
+    """Represents an invite link for a chat.
+
+    See here: https://core.telegram.org/bots/api#chatinvitelink
+    """
+
+    invite_link: str
+    creator: User
+    creates_join_request: bool
+    is_primary: bool
+    is_revoked: bool
+    name: str = None
+    expire_date: int = None
+    member_limit: int = None
+    pending_join_request_count: int = None
+
+
+class ChatJoinRequest(BaseModel):
+    """Represents a join request sent to a chat.
+
+    See here: https://core.telegram.org/bots/api#chatjoinrequest
+    """
+
+    chat: Chat
+    from_: User = Field(alias='from')
+    user_chat_id: int
+    date: int
+    bio: str = None
+    invite_link: ChatInviteLink = None
+
+
+class ChatMemberOwner(BaseModel):
+    """Represents a chat member that owns the chat and has all administrator privileges.
+
+    See here: https://core.telegram.org/bots/api#chatmemberowner
+    """
+
+    status: str
+    user: User
+    is_anonymous: bool
+    custom_title: str = None
+
+
+class ChatMemberAdministrator(BaseModel):
+    """Represents a chat member that has some additional privileges.
+
+    See here: https://core.telegram.org/bots/api#chatmemberadministrator
+    """
+
+    status: str
+    user: User
+    can_be_edited: bool
+    is_anonymous: bool
+    can_manage_chat: bool
+    can_delete_messages: bool
+    can_manage_video_chats: bool
+    can_restrict_members: bool
+    can_promote_members: bool
+    can_change_info: bool
+    can_invite_users: bool
+    can_post_messages: bool = None
+    can_edit_messages: bool = None
+    can_pin_messages: bool = None
+    can_manage_topics: bool = None
+    custom_title: str = None
+
+
+class ChatMemberMember(BaseModel):
+    """Represents a chat member that has no additional privileges or restrictions.
+
+    See here: https://core.telegram.org/bots/api#chatmembermember
+    """
+
+    status: str
+    user: User
+
+
+class ChatMemberRestricted(BaseModel):
+    """Represents a chat member that is under certain restrictions in the chat. Supergroups only.
+
+    See here: https://core.telegram.org/bots/api#chatmemberrestricted
+    """
+
+    status: str
+    user: User
+    is_member: bool
+    can_send_messages: bool
+    can_send_audios: bool
+    can_send_documents: bool
+    can_send_photos: bool
+    can_send_videos: bool
+    can_send_video_notes: bool
+    can_send_voice_notes: bool
+    can_send_polls: bool
+    can_send_other_messages: bool
+    can_add_web_page_previews: bool
+    can_change_info: bool
+    can_invite_users: bool
+    can_pin_messages: bool
+    can_manage_topics: bool
+    until_date: int
+
+
+class ChatMemberLeft(BaseModel):
+    """Represents a chat member that isn't currently a member of the chat, but may join it themselves.
+
+    See here: https://core.telegram.org/bots/api#chatmemberleft
+    """
+
+    status: str
+    user: User
+
+
+class ChatMemberBanned(BaseModel):
+    """Represents a chat member that was banned in the chat and can't return to the chat or view chat messages.
+
+    See here: https://core.telegram.org/bots/api#chatmemberbanned
+    """
+
+    status: str
+    user: User
+    until_date: int
+
+
+ChatMember = TypeVar(
+    'ChatMember',
+    ChatMemberOwner,
+    ChatMemberAdministrator,
+    ChatMemberMember,
+    ChatMemberRestricted,
+    ChatMemberLeft,
+    ChatMemberBanned
+)
 
 
 class MessageReplyMarkup(BaseModel):
